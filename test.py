70907a9884918e2f3c2ae001c1f1bc8734a0d81e
mshelestiuk <83302011+Marina000000000000@users.noreply.github.com>
2024-02-18 12:44:18 +0200

    Add index-test (#1)

---
+++ test.py
@@ -0,0 +1,8 @@
+number = input("Type a 3-digit number: ")
+print(number)
+
+first_number = int(number[0])
+second_number = int(number[1])
+third_number = int(number[2])
+result = first_number + second_number + third_number
+print("Result: " + str(result))
