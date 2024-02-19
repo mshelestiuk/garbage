d130cc0082d7e39774573ba1a4023b1c9b983fa3
Marina Shelestiuk <shelestiuk.marina@gmail.com>
2024-02-19 18:26:48 +0200

    Some cool message

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ second_number = int(number[1])
 third_number = int(number[2])
 result = first_number + second_number + third_number
 print("Result: " + str(result))
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+print("Your band name could be " + street + " " + pet)
