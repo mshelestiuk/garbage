2abffa5a46533571afc6cac8bbb8e7298fb851b7
mshelestiuk <83302011+Marina000000000000@users.noreply.github.com>
2024-02-20 21:19:44 +0200

    Day 3 (#2)
    * Some cool message
    
    * Odd or even
    
    * Test-test

--- test.py
+++ test.py
@@ -16,3 +16,28 @@ print("Welcome to the Band Name Generator.")
 street = input("What's the name of the city you grew up in?")
 pet = input("What's your pet's name?")
 print("Your band name could be " + street + " " + pet)
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+print("Your band name could be " + street + " " + pet)
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+print("Your band name could be " + street + " " + pet)
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+print("Your band name could be " + street + " " + pet)
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+print("Your band name could be " + street + " " + pet)
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+print("Your band name could be " + street + " " + pet)
